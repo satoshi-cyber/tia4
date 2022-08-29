0c64ce87ec4d64076e1557eefb980bf1a092ff32
Bersen <bersen@hotmail.com>
2022-08-29 19:13:57 +0200

    test

--- components/video/index.js
+++ components/video/index.js
@@ -9,7 +9,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const videoClassName =
-  "absolute w-full h-screen min-w-full min-h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover";
+  "absolute w-full h-full min-w-full min-h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover";
 
 const VideoPreview = ({ stream }) => {
   const videoRef = useRef(null);
@@ -58,26 +58,30 @@ const RecordView = () => {
       >
         <SwiperSlide>
           {mediaBlobUrl && status === "stopped" && (
-            <video
-              src={mediaBlobUrl}
-              className={videoClassName}
-              muted
-              playsInline
-              loop
-              autoPlay
-            />
+            <div className="flex flex-1 w-full">
+              <video
+                src={mediaBlobUrl}
+                className={videoClassName}
+                muted
+                playsInline
+                loop
+                autoPlay
+              />
+            </div>
           )}
         </SwiperSlide>
         <SwiperSlide>
           {mediaBlobUrl && status === "stopped" && (
-            <video
-              src={mediaBlobUrl}
-              muted
-              className={videoClassName}
-              playsInline
-              loop
-              autoPlay
-            />
+            <div className="flex flex-1 w-full">
+              <video
+                src={mediaBlobUrl}
+                muted
+                className={videoClassName}
+                playsInline
+                loop
+                autoPlay
+              />
+            </div>
           )}
         </SwiperSlide>
       </Swiper>
