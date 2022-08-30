8ceb68ac98635f28e433ac63d91e3993e19e24a1
Bersen <bersen@hotmail.com>
2022-08-30 21:56:47 +0200

    fix

--- components/video/index.js
+++ components/video/index.js
@@ -112,7 +112,7 @@ const VidePlayer = ({ src, index, swiper }) => {
       playsInline
       loop
       autoPlay
-      muted
+      muted={realIndex !== index}
     />
   );
 };
@@ -194,7 +194,10 @@ const RecordView = () => {
                     src={mediaUrls[index]}
                   />
                 ) : (
-                  <VideoPreview key={previewStream.id} stream={previewStream} />
+                  <VideoPreview
+                    key={previewStream?.id}
+                    stream={previewStream}
+                  />
                 )}
                 <p className="absolute w-[80vw] lg:w-[400px] z-10 text-3xl text-gray-100 text-center mt-10 pointer-events-none drop-shadow-md translate-z-0">
                   {question}
