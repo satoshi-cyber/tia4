dcb5360bbbbb29ecee42dd30e0bdd936fcbf762e
Bersen <bersen@hotmail.com>
2022-08-30 11:27:10 +0200

    fix slider

--- components/video/index.js
+++ components/video/index.js
@@ -1,12 +1,13 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation, EffectCreative } from "swiper";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/effect-creative";
 
 const videoClassName =
   "absolute w-full h-screen min-w-full min-h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover";
@@ -29,38 +30,59 @@ const VideoPreview = ({ stream }) => {
 };
 
 const RecordView = () => {
-  const swiper = useRef();
+  const swipeRef = useRef();
+  const [mediaUrls, setMediaUrls] = useState([,]);
 
-  const { status, startRecording, stopRecording, mediaBlobUrl, previewStream } =
-    useReactMediaRecorder({ video: true, askPermissionOnMount: true });
+  const onStop = (boblUrl) => {
+    mediaUrls[swipeRef.current?.swiper?.realIndex] = boblUrl;
+    setMediaUrls(mediaUrls);
+  };
 
-  const onStartRecording = () => {
-    swiper.current.swiper.disable();
+  const { status, startRecording, stopRecording, previewStream } =
+    useReactMediaRecorder({
+      video: true,
+      askPermissionOnMount: true,
+      onStop,
+      stopStreamsOnStop: false,
+    });
+
+  const handleStartRecording = () => {
+    swipeRef.current?.swiper?.disable();
     startRecording();
   };
 
-  const onStopRecording = () => {
-    swiper.current.swiper.enable();
+  const handleStopRecording = () => {
+    swipeRef.current?.swiper?.enable();
     stopRecording();
   };
 
   return (
     <>
-      <VideoPreview stream={previewStream} />
       <Swiper
-        ref={swiper}
+        ref={swipeRef}
         pagination={{
           type: "progressbar",
         }}
         navigation={true}
-        modules={[Pagination, Navigation]}
-        className="absolute flex flex-1 w-full z-10"
+        effect={"creative"}
+        modules={[Pagination, Navigation, EffectCreative]}
+        creativeEffect={{
+          prev: {
+            translate: [0, 0, -400],
+            shadow: true,
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
+        className="absolute flex flex-1 w-full z-10 bg-gray-800"
       >
         <SwiperSlide>
-          {mediaBlobUrl && status === "stopped" && (
+          <VideoPreview stream={previewStream} />
+          {mediaUrls[0] && status === "stopped" && (
             <div className="flex flex-1 w-full">
               <video
-                src={mediaBlobUrl}
+                src={mediaUrls[0]}
                 className="w-full h-screen object-cover"
                 muted
                 playsInline
@@ -71,10 +93,11 @@ const RecordView = () => {
           )}
         </SwiperSlide>
         <SwiperSlide>
-          {mediaBlobUrl && status === "stopped" && (
+          <VideoPreview stream={previewStream} />
+          {mediaUrls[1] && status === "stopped" && (
             <div className="flex flex-1 w-full">
               <video
-                src={mediaBlobUrl}
+                src={mediaUrls[1]}
                 muted
                 className="w-full h-screen object-cover"
                 playsInline
@@ -87,8 +110,8 @@ const RecordView = () => {
       </Swiper>
       <div className="absolute bg-red-200 z-20">
         <p>{status}</p>
-        <button onClick={onStartRecording}>Start Recording</button>
-        <button onClick={onStopRecording}>Stop Recording</button>
+        <button onClick={handleStartRecording}>Start Recording</button>
+        <button onClick={handleStopRecording}>Stop Recording</button>
       </div>
     </>
   );
