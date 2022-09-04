6a4f01ecde3651d7a27e1883d1c84b310f39f130
Bersen <bersen@hotmail.com>
2022-09-04 19:19:51 +0200

    create a job

---
+++ pages/create-job.js
@@ -0,0 +1,128 @@
+import { Input, FormIcon, Select } from "../components/Form";
+import Menu from "../components/Menu";
+import Icon from "../components/Icon";
+
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+
+const labelStyle = "text-sm text-gray-900 text-left w-full mb-2";
+
+export default function CreateJob() {
+  return (
+    <div className="flex flex-1 flex-col w-full justify-center items-center py-28 md:py-16 md:pl-[70px] ">
+      <Menu />
+      <div className="flex flex-col max-w-[600px] w-full px-4 items-center">
+        <p className="text-3xl mb-4 text-gray-900 text-center mb-10">
+          Create a job!
+        </p>
+        <p className={labelStyle}>Job title:</p>
+        <Input
+          type="text"
+          name="fullname"
+          placeholder="Senior software developer"
+          after={<FormIcon name="HiOutlineBriefcase" />}
+        />
+        <p className={labelStyle}>Deadline:</p>
+        <Input
+          type="date"
+          name="fullname"
+          placeholder="Senior software developer"
+        />
+        <p className={labelStyle}>Questions:</p>
+        <div className="border border-gray-300 w-full p-4 rounded-lg mb-4 flex flex-row relative pt-6 shadow-sm">
+          <Icon
+            name="HiXCircle"
+            size={24}
+            className="absolute right-2 top-2 text-gray-600"
+          />
+          <div className="w-full mr-4">
+            <p className={labelStyle}>question:</p>
+            <Input
+              type="text"
+              name="fullname"
+              placeholder="Senior software developer"
+            />
+          </div>
+          <div>
+            <p className={labelStyle}>time:</p>
+            <Select
+              type="text"
+              name="fullname"
+              placeholder="How did you hear about this position?"
+              defaultValue={2000}
+            >
+              <option value={10000}>10 min</option>
+              <option value={5000}>5 min</option>
+              <option value={2000}>2 min</option>
+              <option value={1000}>1 min</option>
+            </Select>
+          </div>
+        </div>
+        <div className="border border-gray-300 w-full p-4 rounded-lg mb-4 flex flex-row relative pt-6 shadow-sm">
+          <Icon
+            name="HiXCircle"
+            size={24}
+            className="absolute right-2 top-2 text-gray-600"
+          />
+          <div className="w-full mr-4">
+            <p className={labelStyle}>question:</p>
+            <Input
+              type="text"
+              name="fullname"
+              placeholder="Senior software developer"
+            />
+          </div>
+          <div>
+            <p className={labelStyle}>time:</p>
+            <Select
+              type="text"
+              name="fullname"
+              placeholder="How did you hear about this position?"
+              defaultValue={2000}
+            >
+              <option value={10000}>10 min</option>
+              <option value={5000}>5 min</option>
+              <option value={2000}>2 min</option>
+              <option value={1000}>1 min</option>
+            </Select>
+          </div>
+        </div>
+        <div className="border border-gray-300 w-full p-4 rounded-lg mb-4 flex flex-row relative pt-6 shadow-sm">
+          <Icon
+            name="HiXCircle"
+            size={24}
+            className="absolute right-2 top-2 text-gray-600"
+          />
+          <div className="w-full mr-4">
+            <p className={labelStyle}>question:</p>
+            <Input
+              type="text"
+              name="fullname"
+              placeholder="Senior software developer"
+            />
+          </div>
+          <div>
+            <p className={labelStyle}>time:</p>
+            <Select
+              type="text"
+              name="fullname"
+              placeholder="How did you hear about this position?"
+              defaultValue={2000}
+            >
+              <option value={10000}>10 min</option>
+              <option value={5000}>5 min</option>
+              <option value={2000}>2 min</option>
+              <option value={1000}>1 min</option>
+            </Select>
+          </div>
+        </div>
+        <button className="border border-gray-300 sticky self-start top-0 bottom-6 w-full p-3 text-sm focus:outline-none rounded-full focus-within:ring-2 focus:ring-opacity-50 ring-purple-200 shadow-sm mb-4">
+          Add a question
+        </button>
+        <button className="sticky self-start top-0 bottom-6 mt-4 bg-gradient-to-r from-purple-500 w-full p-3 text-sm bg-gray-800 text-gray-100 active:bg-indigo-800 focus:outline-none rounded-full focus-within:ring-2 focus:ring-opacity-50 ring-purple-200 shadow-sm">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
--- pages/jobs.js
+++ pages/jobs.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import Menu from "../components/Menu";
 import Icon from "../components/Icon";
+
 import "video-react/dist/video-react.css";
+import Link from "next/link";
 
 export default function Jobs() {
   return (
@@ -13,14 +16,17 @@ export default function Jobs() {
             <p className="text-3xl mb-2">Get more interviews Today!</p>
             <p className="text-lg text-gray-500 mb-10">Get better candidates</p>
             <div className="flex flex-row items-center mb-20">
-              <button className="flex flex-row rounded-full items-center p-2 pr-4 bg-gradient-to-r from-purple-500 bg-purple-800">
-                <Icon
-                  name="HiPlusCircle"
-                  size={30}
-                  className="mr-2 text-white"
-                />
-                <p className="text-lg text-white">Create new job</p>
-              </button>
+              <Link href="/create-job">
+                <button className="flex flex-row rounded-full items-center p-2 pr-4 bg-gradient-to-r from-purple-500 bg-purple-800">
+                  <Icon
+                    name="HiPlusCircle"
+                    size={30}
+                    className="mr-2 text-white"
+                  />
+
+                  <p className="text-lg text-white">Create new job</p>
+                </button>
+              </Link>
             </div>
           </div>
           <div className="grid grid-cols-1 gap-4">
@@ -31,7 +37,7 @@ export default function Jobs() {
               </div>
               <div className="grid grid-cols-2 gap-4 ml-4">
                 <Icon name="HiPencil" size={30} className="text-black" />
-                <Icon name="HiLink" size={30} className="text-black" />
+                <Icon name="HiExternalLink" size={30} className="text-black" />
               </div>
             </div>
             <div className="flex w-full flex-row justify-between items-center border p-4 rounded-lg shadow-sm">
@@ -41,7 +47,7 @@ export default function Jobs() {
               </div>
               <div className="grid grid-cols-2 gap-4 ml-4">
                 <Icon name="HiPencil" size={30} className="text-black" />
-                <Icon name="HiLink" size={30} className="text-black" />
+                <Icon name="HiExternalLink" size={30} className="text-black" />
               </div>
             </div>
             <div className="flex w-full flex-row justify-between items-center border p-4 rounded-lg shadow-sm">
@@ -51,7 +57,7 @@ export default function Jobs() {
               </div>
               <div className="grid grid-cols-2 gap-4 ml-4">
                 <Icon name="HiPencil" size={30} className="text-black" />
-                <Icon name="HiLink" size={30} className="text-black" />
+                <Icon name="HiExternalLink" size={30} className="text-black" />
               </div>
             </div>
           </div>
