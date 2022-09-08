e6faab67c8ba8de0844d0ffc0b2cb902fd7b65f5
Bersen <bersen@hotmail.com>
2022-09-08 19:16:44 +0200

    oauth

--- pages/oauth-callback.js
+++ pages/oauth-callback.js
@@ -4,5 +4,16 @@ import { Hook } from "../lib";
 import { AuthService } from "../services";
 
 export default function Callback() {
-  return <Hook hookKey={[AuthService.oAuthCallback]} />;
+  return (
+    <div className="flex flex-row w-full justify-center items-center">
+      <Hook hookKey={[AuthService.oAuthCallback]} />
+      <div
+        className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-purple-600 rounded-full"
+        role="status"
+        aria-label="loading"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
 }
