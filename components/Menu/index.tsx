293935c8ea802a6460fea23238db0e1ad576c143
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-11 00:36:18 +0100

    push

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -25,7 +25,7 @@ const Menu = () => {
         {hasCompany ? (
           <>
             <Link href="/jobs" shallow>
-              <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+              <a className="pl-5 py-1 mb-3 flex flex-row items-center text-gray-500 group-one hover:text-gray-800 cursor-pointer transition-all ease-in-out border-r-2 border-purple-800">
                 <Icon
                   name="HiPlusCircle"
                   size={30}
@@ -37,7 +37,7 @@ const Menu = () => {
               </a>
             </Link>
             <Link href="/rate" shallow>
-              <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+              <a className="pl-5 py-1 mb-3 flex flex-row items-center text-gray-500 group-one hover:text-gray-800 cursor-pointer transition-all ease-in-out">
                 <Icon
                   name="HiPlay"
                   size={30}
@@ -49,7 +49,7 @@ const Menu = () => {
               </a>
             </Link>
             <Link href="/archive" shallow>
-              <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+              <a className="pl-5 py-1 mb-3 flex flex-row items-center text-gray-500 group-one hover:text-gray-800 cursor-pointer transition-all ease-in-out">
                 <Icon
                   name="HiArchive"
                   size={30}
@@ -61,7 +61,7 @@ const Menu = () => {
               </a>
             </Link>
             <Link href="/flow" shallow>
-              <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+              <a className="pl-5 py-1 mb-3 flex flex-row items-center text-gray-500 group-one hover:text-gray-800 cursor-pointer transition-all ease-in-out ">
                 <Icon
                   name="HiFilter"
                   size={30}
@@ -73,14 +73,14 @@ const Menu = () => {
               </a>
             </Link>
             <Link href="/company" shallow>
-              <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-gray-800 cursor-pointer">
+              <a className="pl-5 py-2 mb-3 flex flex-row items-center text-gray-500 group-one hover:text-gray-800 cursor-pointer transition-all ease-in-out">
                 <Avatar src="/twiter.png" size="30" round />
-                <span className="ml-5 transition-all ease-in-out absolute w-[240px] left-[69px] group-hover:left-[40px] flex-1">
-                  Lorem LLC
+                <span className="ml-5 transition-all ease-in-out absolute w-[190px] left-[69px] group-hover:left-[40px] flex flex-row items-center">
+                  <span className="flex flex-1">Lorem LLC</span>
+                  <button className="border-l mx-4 p-2 text-gray-500 hover:text-purple-800">
+                    <Icon name="HiSwitchVertical" size={16} />
+                  </button>
                 </span>
-                <button className="border-l rounded-full mr-2 p-2 text-gray-500 hover:text-purple-800">
-                  <Icon name="HiSwitchVertical" size={14} />
-                </button>
               </a>
             </Link>
           </>
@@ -90,7 +90,7 @@ const Menu = () => {
               <Icon
                 name="HiBriefcase"
                 size={30}
-                className="text-gray-500 group-one-hover:text-purple-800 flex-none"
+                className="text-gray-500 group-one-hover:text-purple-800 flex-none transition-all ease-in-out"
               />
               <span className="ml-5 transition-all ease-in-out absolute w-[240px] left-[69px] group-hover:left-[40px]">
                 Post a job
@@ -100,11 +100,11 @@ const Menu = () => {
         )}
         <hr className="mb-4" />
         <Link href="/my-videos" shallow>
-          <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+          <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer transition-all ease-in-out">
             <Icon
               name="HiUserCircle"
               size={30}
-              className="text-gray-500 group-one-hover:text-purple-800"
+              className="text-gray-500 group-one-hover:text-purple-800 flex-none transition-all ease-in-out"
             />
             <span className="ml-5 transition-all ease-in-out absolute w-[240px] left-[69px] group-hover:left-[40px]">
               My Videos
@@ -112,11 +112,11 @@ const Menu = () => {
           </a>
         </Link>
         <Link href="/record-an-interview" shallow>
-          <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+          <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer transition-all ease-in-out">
             <Icon
               name="HiVideoCamera"
               size={30}
-              className="text-gray-500 group-one-hover:text-purple-800"
+              className="text-gray-500 group-one-hover:text-purple-800 flex-none transition-all ease-in-out"
             />
             <span className="ml-5 transition-all ease-in-out absolute w-[240px] left-[69px] group-hover:left-[40px]">
               Record
@@ -124,11 +124,11 @@ const Menu = () => {
           </a>
         </Link>
         <Link href="/info" shallow>
-          <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer">
+          <a className="pl-5 mb-5 flex flex-row items-center text-gray-500 group-one hover:text-purple-800 cursor-pointer  transition-all ease-in-out">
             <Icon
               name="HiInformationCircle"
               size={30}
-              className="text-gray-500 group-one-hover:text-purple-800"
+              className="text-gray-500 group-one-hover:text-purple-800 flex-none transition-all ease-in-out"
             />
             <span className="ml-5 transition-all ease-in-out absolute w-[240px] left-[69px] group-hover:left-[40px]">
               Help and support
