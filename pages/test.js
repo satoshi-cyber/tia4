2200e2fb1426eac0460b1b60a27d9131f0558e47
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-11 02:00:03 +0100

    change loading view

--- pages/test.js
+++ pages/test.js
@@ -1,49 +1,63 @@
 const Test = () => {
   return (
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-sm">
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="username"
-        >
-          Username
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
-          type="text"
-          required
-        />
+    <div id="root">
+      <div data-mg-theme="light">
+        <div>
+          <div
+            style={{ justifyContent: 'space-between', flexDirection: 'column' }}
+          >
+            <div>
+              <div style={{ justifyContent: 'center', flexShrink: 0 }}>
+                <div>
+                  <div
+                    className="mg_fj mg_fl"
+                    aria-live="assertive"
+                    style={{ alignItems: 'center', flexDirection: 'column' }}
+                  >
+                    <div
+                      aria-hidden="true"
+                      style={{
+                        display: 'block',
+                        width: '100%',
+                        height: '10vh',
+                      }}
+                    />
+                    <div className="_a">
+                      <span
+                        className="mg_fx mg_fy _b"
+                        aria-hidden="true"
+                        aria-label="Loading"
+                      >
+                        <svg
+                          width={64}
+                          height={64}
+                          viewBox="0 0 64 64"
+                          fill="none"
+                          stroke="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <circle
+                            cx={32}
+                            cy={32}
+                            r={29}
+                            strokeDasharray="201.06192982974676"
+                            strokeDashoffset="92.4247719318987"
+                            strokeWidth={6}
+                            strokeLinecap="round"
+                            stroke="#9E51EC"
+                            strokeOpacity={1}
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="password"
-        >
-          Password
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          required
-        />
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Sign In
-        </button>
-        <a
-          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-          href="#"
-        >
-          Forgot Password?
-        </a>
-      </div>
-    </form>
+    </div>
   )
 }
 
