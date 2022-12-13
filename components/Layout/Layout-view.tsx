acf78e30378c61ddfaacee1d515f6684297eb2ff
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-13 18:44:37 +0100

    fix layout

--- components/Layout/Layout-view.tsx
+++ components/Layout/Layout-view.tsx
@@ -7,17 +7,17 @@ import Logo from '../../public/logo.svg'
 const CenterLayout: React.FC<LayoutProps> = ({ children }) => (
   <div className={`flex flex-1 w-full justify-center items-center py-20`}>
     <div
-      className={`p-6 max-w-[480px] w-full flex flex-col justify-center items-center`}
+      className={`p-4 md:p-6 max-w-[480px] w-full flex flex-col justify-center items-center`}
     >
-      <Logo className="absolute top-6 left-6" width={120} />
+      <Logo className="absolute top-4 left-4 md:top-6 md:left-6" width={120} />
       {children}
     </div>
   </div>
 )
 
 const Default: React.FC<LayoutProps> = ({ children }) => (
-  <div className="flex flex-1 flex-col w-full items-center py-8 pt-28 md:py-16 md:pl-[70px]">
-    <div className="flex flex-col max-w-[600px] w-full px-4 items-center">
+  <div className="flex flex-1 flex-col w-full items-center py-10 pt-28 md:py-16 md:pl-[70px]">
+    <div className="flex flex-col max-w-[600px] w-full px-6 items-center">
       {children}
     </div>
   </div>
