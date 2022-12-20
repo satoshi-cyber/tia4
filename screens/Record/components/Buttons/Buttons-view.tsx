ccbcd67555d10a1991b3eca2581b21675a55f94c
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-20 18:24:43 +0100

    fix spacing

--- screens/Record/components/Buttons/Buttons-view.tsx
+++ screens/Record/components/Buttons/Buttons-view.tsx
@@ -30,7 +30,7 @@ const VideoPreview: React.FC<ButtonsProps> = ({
     return (
       <>
         <Logo className="absolute m-6" width={120} />
-        <p className="absolute w-[50vw] lg:w-[400px] z-10 text-2xl md:text-3xl text-gray-100 text-center pointer-events-none drop-shadow-md transform-gpu top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p className="absolute w-[80vw] lg:w-[400px] z-10 text-2xl md:text-3xl text-gray-100 text-center pointer-events-none drop-shadow-md transform-gpu top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           Click the button to upload the interview! Good luck!
         </p>
         <div
