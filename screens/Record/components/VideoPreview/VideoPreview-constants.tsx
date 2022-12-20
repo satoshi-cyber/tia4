8dc1787c7d63ebbb30467b62eda1ffa46a02d8b6
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-20 16:45:05 +0100

    remove scale

--- screens/Record/components/VideoPreview/VideoPreview-constants.tsx
+++ screens/Record/components/VideoPreview/VideoPreview-constants.tsx
@@ -1,6 +1,6 @@
 export const VIDEO_PROPS = {
   className:
-    'absolute w-screen h-screen top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover scale-x-flip scale-110',
+    'absolute w-screen h-screen top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover scale-x-flip',
   playsInline: true,
   controls: false,
   autoPlay: true,
