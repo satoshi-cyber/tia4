0329697df1155e0bcfed50e6e40773485d37838b
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-22 17:16:05 +0100

    fix

--- screens/Record/components/Error/Error-constants.ts
+++ screens/Record/components/Error/Error-constants.ts
@@ -3,7 +3,7 @@ export const CLASS_NAMES = {
     'bg-gray-900 w-full min-h-screen flex items-center flex-col justify-center',
   title: "text-2xl text-normal text-white text-center mb-1",
   error: "text-sm text-gray-200 whitespace-pre-line text-center mb-2",
-  description: "text-xl text-white whitespace-pre-line text-center maw-w-[80vw] md:max-w-[400px]"
+  description: "text-xl text-white whitespace-pre-line text-center max-w-[80vw] md:max-w-[400px]"
 }
 
 export const TITLE = 'Error!'
