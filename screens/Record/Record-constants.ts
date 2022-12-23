e7f5297d0b7748f94d288e8f8b1dc24fabe55f7c
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2022-12-23 14:06:02 +0100

    test mobile

--- screens/Record/Record-constants.ts
+++ screens/Record/Record-constants.ts
@@ -7,7 +7,7 @@ export const CLASS_NAMES = {
   slide: "flex flex-1 w-full h-screen relative justify-center bg-gray-900",
   questionWrapper: "absolute z-10 w-full transform-gpu flex flex-col items-center backdrop-blur-2xl bg-black/10 p-4",
   question: {
-    base: 'text-white text-center pointer-events-none transition-all transform-gpu subpixel-antialiased',
+    base: 'text-white text-center pointer-events-none transition-all transform-gpu duration-700 antialiased',
     recording: 'scale-75',
     default: 'text-xl md:text-3xl'
   }
