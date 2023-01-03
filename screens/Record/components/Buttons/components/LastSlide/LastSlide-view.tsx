468793acf7a5c891fc9ae3dca125536b4e35903f
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-03 14:41:43 +0100

    circular progress

--- screens/Record/components/Buttons/components/LastSlide/LastSlide-view.tsx
+++ screens/Record/components/Buttons/components/LastSlide/LastSlide-view.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { ButtonIcon } from '@/components';
+import React, { useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import { ButtonIcon, Icon } from '@/components';
+import 'react-circular-progressbar/dist/styles.css';
 
 import { CLASS_NAMES, DESCRIPTION } from './LastSlide-constants';
 
 import Logo from '../../../../../../public/logo-white.svg';
 
-const Loading: React.FC = () => (
-  <>
-    <Logo className={CLASS_NAMES.logo} width={120} />
-    <p className={CLASS_NAMES.description}>{DESCRIPTION}</p>
-    <div className={CLASS_NAMES.buttonContainer}>
-      <div className={CLASS_NAMES.uploadButton}>
-        <ButtonIcon name="HiUpload" size={24} />
+const Loading: React.FC = () => {
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleUpload = () => {
+    setIsUploading(true);
+  };
+
+  return (
+    <>
+      <Logo className={CLASS_NAMES.logo} width={120} />
+      <p className={CLASS_NAMES.description}>{DESCRIPTION}</p>
+      <div className={CLASS_NAMES.buttonContainer}>
+        {isUploading ? (
+          <div className={CLASS_NAMES.progressContainer}>
+            <CircularProgressbar value={0} text={`${0}%`} background={false} />
+          </div>
+        ) : (
+          <button className={CLASS_NAMES.uploadButton} onClick={handleUpload}>
+            <Icon name="HiUpload" size={24} className={CLASS_NAMES.icon} />
+          </button>
+        )}
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Loading;
