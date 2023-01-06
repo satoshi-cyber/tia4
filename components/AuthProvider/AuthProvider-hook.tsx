db3471e4bdbc0b82fd5135161fa3fec0c3996f54
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-06 16:51:13 +0100

    remove cookie

--- components/AuthProvider/AuthProvider-hook.tsx
+++ components/AuthProvider/AuthProvider-hook.tsx
@@ -3,16 +3,15 @@ import { useCallback } from 'react';
 import { useCookies } from 'react-cookie';
 
 export const useAuthProvider = () => {
-  const [{ [TOKEN_COOKIE_KEY]: token }, setCookie, removeCookie] = useCookies([
-    TOKEN_COOKIE_KEY,
-  ]);
+  const [cookies, setCookie, removeCookie] = useCookies();
+
+  const token = cookies[TOKEN_COOKIE_KEY];
 
   const setToken = useCallback((cookieToken?: string) => {
     console.log({ cookieToken });
 
     if (!cookieToken) {
-      removeCookie(TOKEN_COOKIE_KEY);
-      console.log('cookie removed');
+      removeCookie(TOKEN_COOKIE_KEY, { path: '/' });
 
       return;
     }
