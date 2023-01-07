71082bffd44e40d090124fe5b76701a64bc24523
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-07 23:27:33 +0100

    smaller text

--- components/SocialButton/index.js
+++ components/SocialButton/index.js
@@ -1,8 +1,8 @@
 const SocialButton = ({ title, provider, ...restProps }) => (
   <button
     {...restProps}
-    className={`p-4 text-xs bg-gray-800 text-gray-100 focus:outline-none rounded-full  transition-all ease-in-out disabled:opacity-80 ${
-      provider === "linkedin" ? "bg-[#1877F2]" : "bg-[#2766C2]"
+    className={`p-4 text-[10px] md:text-xs bg-gray-800 text-gray-100 focus:outline-none rounded-full  transition-all ease-in-out disabled:opacity-80 ${
+      provider === 'linkedin' ? 'bg-[#1877F2]' : 'bg-[#2766C2]'
     }`}
   >
     {title}
