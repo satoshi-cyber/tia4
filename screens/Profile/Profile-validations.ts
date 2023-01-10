b6c7015fb0762763cd4adfd93c8610959128225f
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-10 23:47:07 +0100

    fix nullable

--- screens/Profile/Profile-validations.ts
+++ screens/Profile/Profile-validations.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const updateProfileSchema = yup.object({
-  firstName: yup.string().required("You must enter your name"),
-  lastName: yup.string().required("You must enter your last name"),
+  firstName: yup.string().nullable().required("You must enter your name"),
+  lastName: yup.string().nullable().required("You must enter your last name"),
   linkedInProfile: yup.string().nullable().url("You must provide a valid url"),
 })
