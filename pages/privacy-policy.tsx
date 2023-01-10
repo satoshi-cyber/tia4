b9c773feeb30a259d3de1314409e3fa369a7a71a
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-10 13:55:23 +0100

    privacy policy

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,58 @@
+import { Layout, Markdown } from '@/components';
+
+const Policy = () => (
+  <Layout.Apply>
+    <Markdown
+      text={`## Privacy Policy
+
+Last updated: 10/01/2023
+
+At "The Interview", we are committed to protecting the privacy of our users and their personal information. This privacy policy (the "Policy") explains how we collect, use, and share personal information that we obtain through our mobile application and website (collectively, the "Service"). By using the Service, you consent to the collection, use, and sharing of your personal information as described in this Policy.
+
+Information We Collect
+
+When you use the Service, we may collect the following types of information:
+
+*   Personal information, such as your name, email address, and phone number, that you provide to us when you create an account or contact us for support.
+*   Information about your device, such as the device type, operating system, and unique device ID, as well as information about your use of the Service, such as the pages you visit and the features you use.
+*   Information about your location, such as your IP address and device location, if you use the Service's location-enabled features.
+*   Audio and video recording of the interview conducted through the app.
+
+How We Use Your Information
+
+We use the information we collect from you to provide and improve the Service, and for the following purposes:
+
+*   To provide you with the features and services that you request, such as the ability to record and store interview recordings.
+*   To respond to your requests for support and to provide customer service.
+*   To personalize your experience on the Service and to send you notifications about your account or activity on the Service that may be of interest to you.
+*   To improve the Service and develop new features.
+
+Sharing of Your Information
+
+We do not sell or share your personal information with third parties for their own marketing or advertising purposes without your consent. However, we may share your information as described below:
+
+*   With service providers that perform functions on our behalf, such as hosting the Service, conducting quality assurance testing, or providing customer service.
+*   With third parties in connection with a merger, acquisition, or sale of all or a portion of our assets.
+*   If we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request.
+*   If we are required by law to disclose information or if we have a good faith belief that such action is necessary to comply with legal requirements or with legal process served on us.
+
+Security
+
+We take reasonable precautions to protect your personal information from loss, misuse, unauthorized access, disclosure, alteration and destruction. However, no internet or e-mail transmission is ever fully secure or error free. In particular, e-mail sent to or from the Service may not be secure. Therefore, you should take special care in deciding what information you send to us via e-mail.
+
+Accessing and Changing Your Personal Information
+
+You can review and change your personal information at any time by logging into your account and updating your profile. If you want to delete your account, please contact us.
+
+International Transfer of Information
+
+The Service is operated in the United States and intended for users in the United States. If you are using the Service from outside the United States, please be aware that your information may be transferred to, stored, and processed in the United States, where our servers are located and our central database is operated. By using the Service, you consent to the transfer of your information to the United States.
+
+Changes to this Policy
+
+We may change this Policy from time to time. If we make any changes, we will update the "Last Updated" date`}
+    />
+  </Layout.Apply>
+);
+
+export default Policy;
