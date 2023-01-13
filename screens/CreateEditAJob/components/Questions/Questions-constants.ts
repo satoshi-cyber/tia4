81ca1f5a0617336f7f2fafa142cd36e466360e58
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-13 15:53:17 +0100

    fix sizes

--- screens/CreateEditAJob/components/Questions/Questions-constants.ts
+++ screens/CreateEditAJob/components/Questions/Questions-constants.ts
@@ -28,10 +28,11 @@ export const TIME_FIELD_PROPS = {
   label: "Time:",
   placeholder: "2 min",
   options: [
-    { value: "600", label: '10 min' },
     { value: "300", label: '5 min' },
+    { value: "180", label: '3 min' },
     { value: "120", label: '2 min' },
     { value: "60", label: '1 min' },
+    { value: "30", label: '30 sec' },
   ]
 }
 
