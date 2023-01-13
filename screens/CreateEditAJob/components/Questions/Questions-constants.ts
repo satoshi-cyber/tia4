4d23efa8c74682d7dc5044c7fe86b2694ccd0057
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-13 16:31:00 +0100

    2 minutes max

--- screens/CreateEditAJob/components/Questions/Questions-constants.ts
+++ screens/CreateEditAJob/components/Questions/Questions-constants.ts
@@ -28,7 +28,6 @@ export const TIME_FIELD_PROPS = {
   label: "Time:",
   placeholder: "2 min",
   options: [
-    { value: "180", label: '3 min' },
     { value: "120", label: '2 min' },
     { value: "60", label: '1 min' },
     { value: "30", label: '30 sec' },
