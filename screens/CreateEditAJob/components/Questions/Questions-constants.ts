54c3088c26a142eb2be5fb0221a1e15192d1e437
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-17 15:55:22 +0100

    fix duplicate 3 min

--- screens/CreateEditAJob/components/Questions/Questions-constants.ts
+++ screens/CreateEditAJob/components/Questions/Questions-constants.ts
@@ -30,7 +30,6 @@ export const TIME_FIELD_PROPS = {
   options: [
     { value: "300", label: '5 min' },
     { value: "180", label: '3 min' },
-    { value: "180", label: '3 min' },
     { value: "120", label: '2 min' },
     { value: "60", label: '1 min' },
     { value: "30", label: '30 sec' },
