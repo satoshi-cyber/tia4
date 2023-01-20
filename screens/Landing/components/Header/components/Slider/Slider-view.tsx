1085893368cd594cd46eca91c17ac09ef751e090
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-20 22:48:37 +0100

    youtube

--- screens/Landing/components/Header/components/Slider/Slider-view.tsx
+++ screens/Landing/components/Header/components/Slider/Slider-view.tsx
@@ -7,13 +7,13 @@ const Slider = () => {
       <ReactCompareSlider
         className="w-full h-full"
         itemOne={
-          <video
-            src="/business.mp4"
-            playsInline
-            muted
-            autoPlay
-            loop
-            className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover"
+          <iframe
+            className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
+            src={`https://www.youtube.com/embed/e5wsBHP5Ino?rel=0&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
           />
         }
         itemTwo={
