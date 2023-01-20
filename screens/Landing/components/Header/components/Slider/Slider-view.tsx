3f207bdd6091997a809284b1ba9e1fd6b3cc8a78
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-21 00:31:07 +0100

    cdn

--- screens/Landing/components/Header/components/Slider/Slider-view.tsx
+++ screens/Landing/components/Header/components/Slider/Slider-view.tsx
@@ -1,42 +1,25 @@
 'use client';
-import { useEffect, useState } from 'react';
 import { ReactCompareSlider } from 'react-compare-slider';
 
 const Slider = () => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(true);
-    }, 300);
-  }, []);
-
-  if (!loading) return <div className="relative w-full h-full bg-black"></div>;
-
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-full bg-black">
       <ReactCompareSlider
         className="w-full h-full"
         itemOne={
-          <iframe
-            loading="lazy"
+          <video
+            muted
+            autoPlay
+            src="https://s3.tebi.io/tia/business.mp4"
             className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
-            src={`https://www.youtube.com/embed/e5wsBHP5Ino?rel=0&playlist=e5wsBHP5Ino&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
           />
         }
         itemTwo={
-          <iframe
-            loading="lazy"
+          <video
+            muted
+            autoPlay
+            src="videoplayback.mp4"
             className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
-            src={`https://www.youtube.com/embed/YN0Q4NjPi8Y?rel=0&playlist=YN0Q4NjPi8Y&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
           />
         }
       />
