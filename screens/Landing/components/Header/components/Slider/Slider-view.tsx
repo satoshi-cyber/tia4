ec600efa1c27b5c77199fc4999bcc9b38de8b3de
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-20 23:01:54 +0100

    fix loop

--- screens/Landing/components/Header/components/Slider/Slider-view.tsx
+++ screens/Landing/components/Header/components/Slider/Slider-view.tsx
@@ -9,7 +9,7 @@ const Slider = () => {
         itemOne={
           <iframe
             className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
-            src={`https://www.youtube.com/embed/e5wsBHP5Ino?rel=0&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
+            src={`https://www.youtube.com/embed/e5wsBHP5Ino?rel=0&playlist=e5wsBHP5Ino&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -19,7 +19,7 @@ const Slider = () => {
         itemTwo={
           <iframe
             className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
-            src={`https://www.youtube.com/embed/YN0Q4NjPi8Y?rel=0&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
+            src={`https://www.youtube.com/embed/YN0Q4NjPi8Y?rel=0&playlist=YN0Q4NjPi8Y&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
