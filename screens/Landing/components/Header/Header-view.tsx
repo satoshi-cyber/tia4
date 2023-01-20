bcb52a0e379831737d6f56f590876019ef57ffd5
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-20 16:48:43 +0100

    video

--- screens/Landing/components/Header/Header-view.tsx
+++ screens/Landing/components/Header/Header-view.tsx
@@ -1,3 +1,8 @@
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from 'react-compare-slider';
+
 export const Header: React.FC = () => (
   <div className={`flex flex-col md:flex-row `}>
     <h2 className={`text-left text-2xl font-thin pb-2 md:w-1/6`}>
@@ -7,7 +12,40 @@ export const Header: React.FC = () => (
       more efficient way
     </h2>
     <div className={`mt-16 scale-105 md:scale-100 md:mt-0 md:w-5/6 `}>
-      <img src="/mac.svg" alt="Mac computer" />
+      <div className="relative">
+        <div className="absolute top-[5.7%] left-[13%] z-10 w-[73.7%] h-[75.1%]">
+          <div className="relative w-full h-full">
+            <ReactCompareSlider
+              className="w-full h-full"
+              itemOne={
+                <video
+                  src="/business.mp4"
+                  playsInline
+                  muted
+                  autoPlay
+                  loop
+                  className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover"
+                />
+              }
+              itemTwo={
+                <video
+                  src="/videoplayback.mp4"
+                  playsInline
+                  muted
+                  autoPlay
+                  loop
+                  className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover"
+                />
+              }
+            />
+          </div>
+        </div>
+        <img
+          src="/mac.svg"
+          alt="Mac computer"
+          className="z-10 relative pointer-events-none"
+        />
+      </div>
     </div>
   </div>
 );
