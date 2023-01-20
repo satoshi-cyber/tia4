db31b536a68a103911258d31b67cf32135368085
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-21 00:19:17 +0100

    timeout

--- screens/Landing/components/Header/components/Slider/Slider-view.tsx
+++ screens/Landing/components/Header/components/Slider/Slider-view.tsx
@@ -1,13 +1,25 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { ReactCompareSlider } from 'react-compare-slider';
 
 const Slider = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 300);
+  }, []);
+
+  if (!loading) return <div className="relative w-full h-full bg-black"></div>;
+
   return (
     <div className="relative w-full h-full">
       <ReactCompareSlider
         className="w-full h-full"
         itemOne={
           <iframe
+            loading="lazy"
             className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
             src={`https://www.youtube.com/embed/e5wsBHP5Ino?rel=0&playlist=e5wsBHP5Ino&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
             frameBorder="0"
@@ -18,6 +30,7 @@ const Slider = () => {
         }
         itemTwo={
           <iframe
+            loading="lazy"
             className="absolute w-full h-full top-1/2 left-1/2 transition-all transform -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
             src={`https://www.youtube.com/embed/YN0Q4NjPi8Y?rel=0&playlist=YN0Q4NjPi8Y&autoplay=1&mute=1&controls=0&showinfo=0&loop=1`}
             frameBorder="0"
