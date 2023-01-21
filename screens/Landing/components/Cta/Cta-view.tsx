fa11ba2685d46f10f9fec6bfa27270b151baab99
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-21 03:00:15 +0100

    fix

--- screens/Landing/components/Cta/Cta-view.tsx
+++ screens/Landing/components/Cta/Cta-view.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 const Cta: React.FC = () => (
   <div className="container mx-auto flex flex-col justify-center items-center mb-24">
     <div className="text-center max-w-[300px]">
-      <h5 className="font-light text-3xl mb-8">
+      <p className="font-light text-3xl mb-8">
         Experience the power of The Interview
-      </h5>
+      </p>
     </div>
     <div>
       <Link href={URLS.LOGIN} prefetch={false}>
