312ffa0df22c10f60ee19c52f18c1588cbdd01b6
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-01-22 18:13:01 +0100

    link to landing

--- components/Layout/Layout-view.tsx
+++ components/Layout/Layout-view.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 import { LayoutProps } from './Layout-types';
 
 import Logo from '../../public/logo.svg';
+import Link from 'next/link';
+import { URLS } from '@/config';
 
 const CenterLayout: React.FC<LayoutProps> = ({ children }) => (
   <div className={`flex flex-1 w-full justify-center items-center py-20 `}>
     <div
       className={`p-4 md:p-6 max-w-[480px] w-full flex flex-col justify-center items-center`}
     >
-      <Logo className="absolute top-4 left-4 md:top-6 md:left-6" width={120} />
+      <Link href={URLS.LANDING}>
+        <Logo
+          className="absolute top-4 left-4 md:top-6 md:left-6"
+          width={120}
+        />
+      </Link>
       {children}
     </div>
   </div>
@@ -20,7 +27,12 @@ const Apply: React.FC<LayoutProps> = ({ children }) => (
     className={`flex flex-1 flex-col w-full items-center py-10 pt-28 md:py-16 md:pl-[70px] `}
   >
     <div className="flex flex-col max-w-[600px] w-full p-4 md:p-6 items-center">
-      <Logo className="absolute top-4 left-4 md:top-6 md:left-6" width={120} />
+      <Link href={URLS.LANDING}>
+        <Logo
+          className="absolute top-4 left-4 md:top-6 md:left-6"
+          width={120}
+        />
+      </Link>
       {children}
     </div>
   </div>
