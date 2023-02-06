974e1df6ef6be3857f27ba8961c2e7dd117d8d65
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-02-06 22:14:42 +0100

    90%

--- screens/Record/components/Question/Question-constants.tsx
+++ screens/Record/components/Question/Question-constants.tsx
@@ -3,6 +3,6 @@ export const CLASS_NAMES = {
     'absolute z-10 transform-gpu flex flex-col items-center backdrop-blur-2xl bg-black/20 rounded-3xl p-3 m-6 md:max-w-[500px]',
   question: {
     base: 'text-md md:text-lg text-white text-center pointer-events-none transition-all transform-gpu duration-700',
-    isRecording: 'scale-[80%]',
+    isRecording: 'scale-[90%]',
   },
 };
