4a7175de5fd3f47ce96e6f178d7d08c9f5a4c989
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-02-06 16:50:13 +0100

    fix

--- components/SubmitButton/SubmitButton-constants.ts
+++ components/SubmitButton/SubmitButton-constants.ts
@@ -1,3 +1,3 @@
 export const CLASS_NAMES = {
-  button: "bg-gray-800 bg-gradient-to-r from-purple-500 w-full p-3 text-md text-gray-100 active:bg-indigo-800 focus:outline-none rounded-full focus-within:ring-2 focus:ring-opacity-50 ring-purple-200 ease-in-out disabled:from-purple-400 transition-all hover:shadow-button"
+  button: "bg-gray-800 bg-gradient-to-r from-purple-500 w-full py-[10px] px-3 text-md text-gray-100 active:bg-indigo-800 focus:outline-none rounded-full focus-within:ring-2 focus:ring-opacity-50 ring-purple-200 ease-in-out disabled:from-purple-400 transition-all hover:shadow-button"
 }
