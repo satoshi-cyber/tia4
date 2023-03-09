1b8cdcd1211a778b96871f22499cd6505d9991a0
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-03-09 09:20:40 +0100

    fix

--- screens/Landing/components/Header/Header-view.tsx
+++ screens/Landing/components/Header/Header-view.tsx
@@ -1,14 +1,9 @@
 import Slider from './components/Slider';
 
 export const Header: React.FC = () => (
-  <div className={`flex flex-col md:flex-row `}>
-    <h2 className={`text-left text-2xl font-thin pb-8 md:pb-0 md:w-1/6`}>
-      Say <span className={`font-normal`}>goodbye</span> to endless resumes and
-      in-person interviews, and{' '}
-      <span className={`font-normal text-purple-800`}>hello</span> to a smarter,
-      more efficient way
-    </h2>
-    <div className={`mt-16 scale-105 md:scale-100 md:mt-0 md:w-5/6 `}>
+  <div className={`flex flex-col`}>
+    <h2 className={`text-center text-5xl mb-20`}>#newagehiring</h2>
+    <div className={`mt-16 `}>
       <div className="relative">
         <div className="absolute top-[5.7%] left-[13%] z-10 w-[73.7%] h-[75.1%]">
           <Slider />
