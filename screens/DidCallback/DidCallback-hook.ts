e5fb80257db9e65f97db0098f4c8aead5eb0a338
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-03-15 02:50:20 +0100

    fix security issue

--- screens/DidCallback/DidCallback-hook.ts
+++ screens/DidCallback/DidCallback-hook.ts
@@ -7,18 +7,30 @@ import nextBase64 from 'next-base64';
 export const useDidCallback = () => {
   const router = useRouter()
 
-  const { authenticateUserFromDid } = useUser()
+  const { authenticateUserFromDid, authenticateUserFromRedirect } = useUser()
 
   useLayoutEffect(() => {
     if (!router.isReady) return;
 
     const from = router.query.from as string
-    const did = router.query.did as string
 
-    authenticateUserFromDid(did).then(() => {
-      const url = from ? nextBase64.decode(from) : `${DOMAIN}${URLS.HOME}`
+    // const href = `https://theinterview.page.link/?link=${url}`
 
-      router.replace(url)
+    authenticateUserFromRedirect().then((did) => {
+
+      if (!did) {
+        router.replace(URLS.LOGIN)
+
+        return
+      }
+
+      authenticateUserFromDid(did).then(() => {
+        const url = from ? nextBase64.decode(from) : `${DOMAIN}${URLS.HOME}`
+
+        router.replace(url)
+      })
+
     })
+
   }, [router])
 };
--- screens/RedirectCallback/RedirectCallback-hook.ts
+++ screens/RedirectCallback/RedirectCallback-hook.ts
@@ -1,24 +1,22 @@
 import { DOMAIN, URLS } from "@/config";
-import { useUser } from "@/hooks";
 import { useRouter } from "next/router";
 import { useLayoutEffect } from "react";
 
 export const useRedirectCallback = () => {
   const router = useRouter()
 
-  const { authenticateUserFromRedirect } = useUser()
-
   useLayoutEffect(() => {
     if (!router.isReady) return;
 
+    const magicCredential = router.query.magic_credential
+
     const from = router.query.from as string
 
-    authenticateUserFromRedirect().then((did) => {
-      const url = encodeURIComponent(from ? `${DOMAIN}${URLS.DID_CALLBACK}/${from}?did=${did}` : `${DOMAIN}${URLS.DID_CALLBACK}?did=${did}`)
+    const url = encodeURIComponent(from ? `${DOMAIN}${URLS.DID_CALLBACK}/${from}?magic_credential=${magicCredential}` : `${DOMAIN}${URLS.DID_CALLBACK}?magic_credential=${magicCredential}`)
 
-      const href = `https://theinterview.page.link/?link=${url}`
+    const href = `https://theinterview.page.link/?link=${url}`
 
-      router.replace(href)
-    })
+    router.replace(href)
+
   }, [router])
 };
