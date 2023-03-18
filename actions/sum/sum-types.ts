1d3f4c578b2077fa7ddd970888bbc8dccb8f7879
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-03-18 08:43:37 +0100

    remove tine

--- actions/sum/sum-types.ts
+++
@@ -1,7 +0,0 @@
-export type TineFactory<P, T> = (payload: P) => (ctx: Object) => Promise<[Object, T?, Error?]>
-
-export type TineVar<T> = { __value: (ctx: Object) => T | undefined }
-export interface SumPayload {
-    a: number | TineVar<number>
-    b: number | TineVar<number>
-}
