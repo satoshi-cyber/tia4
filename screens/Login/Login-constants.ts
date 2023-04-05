30a7538d2d5bc165934dd5e28ea8159a819586f3
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-04-05 15:51:27 +0200

    Fix

--- screens/Login/Login-constants.ts
+++ screens/Login/Login-constants.ts
@@ -2,12 +2,12 @@ export const CLASS_NAMES = {
   form: "w-full",
   seperator: 'text-gray-600 my-5',
   socialButtonsContainer: 'grid grid-cols-1 md:grid-cols-2 gap-4 w-full',
-  embedded: 'text-gray-600'
+  embedded: 'text-gray-600 text-center'
 }
 
 export const SEPERATOR_TEXT = 'OR'
 
-export const EMBEDDED_TEXT = 'To use social logins please open this page from a browser!'
+export const EMBEDDED_TEXT = 'To use social logins please open this page in a browser!'
 
 export const TITLE_PROPS = {
   title: 'Change your life Today!',
