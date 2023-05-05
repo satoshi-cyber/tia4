77217e528a6422ef4fccbe954c6407e2fcfa46d6
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-05-05 04:36:35 +0200

    remove description

--- useCases/jobs/index.ts
+++ useCases/jobs/index.ts
@@ -1,5 +1,10 @@
 import prisma from '@/actions/prisma';
 
-const jobs = prisma.job.findMany({});
+const jobs = prisma.job.findMany({
+    select: {
+        id: true,
+        title: true,
+    }
+});
 
 export default jobs.noInput();
