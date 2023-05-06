3729642b4d9c72be44f5cc7b8e6904ce30efba60
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-05-06 20:22:12 +0200

    better naming

--- actions/condition/index.ts
+++ actions/condition/index.ts
@@ -2,14 +2,14 @@ import { tineAction, TineActionOptions } from 'tinejs';
 
 const condition = tineAction(
   async <P, T = null>(
-    [condition, $if, $else = null]: [boolean, P, (T | null)?],
+    [condition, $true, $false = null]: [boolean, P, (T | null)?],
     { parsePayload, ctx }: TineActionOptions
   ) => {
     if (await parsePayload(ctx, condition)) {
-      return await parsePayload(ctx, $if);
+      return await parsePayload(ctx, $true);
     }
 
-    return ((await parsePayload(ctx, $else)) ?? null) as T;
+    return ((await parsePayload(ctx, $false)) ?? null) as T;
   },
   { action: 'condition', skipParse: true }
 );
