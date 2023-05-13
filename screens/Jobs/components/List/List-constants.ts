97c0f649e8c8fcc7efc3be4a69bc87baef32b165
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-05-13 22:55:11 +0200

    add more skeletons

--- screens/Jobs/components/List/List-constants.ts
+++ screens/Jobs/components/List/List-constants.ts
@@ -1,5 +1,15 @@
 export const CLASS_NAMES = {
-  container: "grid grid-cols-1 gap-4 w-full",
+  container: 'grid grid-cols-1 gap-4 w-full',
 };
 
-export const SKELETON_JOBS = [{ id: 1, title: undefined, deadline: undefined }, { id: 2, title: undefined, deadline: undefined }, { id: 3, title: undefined, deadline: undefined }]
+export const SKELETON_JOBS = [
+  { id: 1, title: undefined, deadline: undefined },
+  { id: 2, title: undefined, deadline: undefined },
+  { id: 3, title: undefined, deadline: undefined },
+  { id: 4, title: undefined, deadline: undefined },
+  { id: 5, title: undefined, deadline: undefined },
+  { id: 6, title: undefined, deadline: undefined },
+  { id: 7, title: undefined, deadline: undefined },
+  { id: 8, title: undefined, deadline: undefined },
+  { id: 9, title: undefined, deadline: undefined },
+];
