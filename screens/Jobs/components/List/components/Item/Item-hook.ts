8177850a86dc7c6656543e50a26bcefcb0df9550
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-05-15 23:20:04 +0200

    fix type

--- screens/Jobs/components/List/components/Item/Item-hook.ts
+++ screens/Jobs/components/List/components/Item/Item-hook.ts
@@ -1,40 +1,38 @@
-import { TOAST_OPTIONS, URLS } from "@/config"
-import { format } from "date-fns";
-import { useRouter } from "next/router"
+import { TOAST_OPTIONS, URLS } from '@/config';
+import { format } from 'date-fns';
+import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 
-import { TOAST_MESSAGE } from "./Item-constants";
+import { TOAST_MESSAGE } from './Item-constants';
 
 interface ItemOptions {
-  jobId: string | number
-  deadline: Date | undefined
+  jobId: string | number;
+  deadline: Date | undefined;
 }
 
 export const useItem = ({ jobId, deadline }: ItemOptions) => {
-  const router = useRouter()
+  const router = useRouter();
 
   const handleEditJob = () => {
-    router.push({ pathname: URLS.JOB, query: { jobId } })
-  }
+    router.push({ pathname: URLS.JOB, query: { jobId } });
+  };
 
   const handleCopyLink = async () => {
     try {
-      const baseUrl = window.location.href.split(window.location.pathname)[0]
+      const baseUrl = window.location.href.split(window.location.pathname)[0];
 
-      const pathname = URLS.PUBLIC_JOB.replace('[applyJobId]', String(jobId))
+      const pathname = URLS.PUBLIC_JOB.replace('[applyJobId]', String(jobId));
 
-      await navigator.clipboard.writeText(`${baseUrl}${pathname}`)
+      await navigator.clipboard.writeText(`${baseUrl}${pathname}`);
 
-      toast.success(TOAST_MESSAGE.success, TOAST_OPTIONS)
-
+      toast.success(TOAST_MESSAGE.success, TOAST_OPTIONS);
     } catch (e) {
-
-      toast.error(TOAST_MESSAGE.error, TOAST_OPTIONS)
-
+      toast.error(TOAST_MESSAGE.error, TOAST_OPTIONS);
     }
-  }
+  };
 
-  const deadlineLabel = deadline && `Deadline: ${format(new Date(deadline), 'MM/dd/yyyy')}`
+  const deadlineLabel =
+    deadline && `Deadline: ${format(deadline, 'MM/dd/yyyy')}`;
 
-  return { handleEditJob, handleCopyLink, deadlineLabel }
-}
+  return { handleEditJob, handleCopyLink, deadlineLabel };
+};
