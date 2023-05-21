9b21163a0b26b64480ed0307809d32131af01bd8
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-05-21 15:24:43 +0200

    import svg

--- screens/Landing/components/index.ts
+++ screens/Landing/components/index.ts
@@ -1,6 +1,6 @@
-export { default as Header } from './Header'
-export { default as Description } from './Description'
-export { default as Mobile } from './Mobile'
-export { default as Security } from './Security'
-export { default as Testimonials } from './Testimonials'
-export { default as Cta } from './Cta'
+export { default as Header } from './Header';
+export { default as Description } from './Description';
+export { default as Mobile } from './Mobile';
+export { default as Security } from './Security';
+export { default as Testimonials } from './Testimonials';
+export { default as Cta } from './Cta';
