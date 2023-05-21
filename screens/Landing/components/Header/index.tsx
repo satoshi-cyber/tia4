a3ba876e5bbd1e15a710c5b005dab83efed31e23
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-05-21 15:51:15 +0200

    fix

--- screens/Landing/components/Header/index.tsx
+++ screens/Landing/components/Header/index.tsx
@@ -2,5 +2,5 @@ import dynamic from 'next/dynamic';
 
 export default dynamic(() => import('./Header-view'), {
   ssr: false,
-  loading: () => <div className="w-full aspect-[976/600]" />,
+  loading: () => <div className="w-full aspect-[976/872]" />,
 });
