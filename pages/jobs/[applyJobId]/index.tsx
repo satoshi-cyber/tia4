e1cddc4d5e8c31ae72e3160d4265c5ace0f669bc
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-06-02 01:45:11 +0200

    fix

--- pages/jobs/[applyJobId]/index.tsx
+++ pages/jobs/[applyJobId]/index.tsx
@@ -1,10 +1,11 @@
-// import { useSWRProps } from 'tinejs.next';
+import { useSWRProps } from 'tinejs.next';
 import { GetServerSideProps } from 'next';
-// import publicJob from '@/useCases/publicJob';
+import publicJob from '@/useCases/publicJob';
 import withSWRFallback from '@/hocs/withSWRFallback';
 
 import PublicJob from '../../../screens/PublicJob';
-// import { TOKEN_COOKIE_KEY } from '@/config/auth';
+import { TOKEN_COOKIE_KEY } from '@/config/auth';
+import { tineCtx } from 'tinejs';
 
 export const runtime = 'experimental-edge';
 
@@ -13,17 +14,28 @@ export default withSWRFallback(PublicJob);
 export const getServerSideProps: GetServerSideProps<{ fallback: any }> = async (
   ctx
 ) => {
-  console.log(ctx.req.headers);
-  // const jobId = String(ctx.query.applyJobId);
+  const headers = new Map();
 
-  // const publicDataProps = ctx.req?.cookies[TOKEN_COOKIE_KEY]
-  //   ? {}
-  //   : await useSWRProps(publicJob, { id: jobId });
+  headers.set('X-Vercel-IP-Latitude', ctx.req.headers['x-vercel-ip-latitude']);
+  headers.set(
+    'X-Vercel-IP-Longitude',
+    ctx.req.headers['x-vercel-ip-longitude']
+  );
+
+  const jobId = String(ctx.query.applyJobId);
+
+  const publicDataProps = ctx.req?.cookies[TOKEN_COOKIE_KEY]
+    ? {}
+    : await useSWRProps(
+        publicJob,
+        { id: jobId },
+        { ctx: tineCtx({ headers }) }
+      );
 
   return {
     props: {
       fallback: {
-        // ...publicDataProps,
+        ...publicDataProps,
       },
     },
   };
