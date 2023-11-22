bab5b8ac0a4ad9f395128bbf064a7031bfba1b11
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-11-22 09:33:01 +0100

    fix

--- screens/SetupCompany/SetupCompany-view.tsx
+++ screens/SetupCompany/SetupCompany-view.tsx
@@ -3,6 +3,7 @@ import Layout from '@/components/Layout';
 import { Field, Form, FormIcon } from '@/components/Form';
 import SubmitButton from '@/components/SubmitButton';
 import Loader from '@/components/Loader';
+import Title from '@/components/Title';
 import Text from '@/components/Text';
 
 import {
@@ -10,21 +11,22 @@ import {
   COMPANY_WEBSITE_FIELD_PROPS,
   SUBMIT_BUTTON_PROPS,
   CLASS_NAMES,
-  // TITLE_PROPS,
+  TITLE_PROPS,
   INFO_PROPS,
 } from './SetupCompany-constants';
 import { useSetupCompany } from './SetupCompany-hook';
 import { SetupCompanyProps } from './SetupCompany-types';
+import ButtonIcon from '@/components/ButtonIcon';
 
 const SetupCompany: React.FC<SetupCompanyProps> = ({ showSkip }) => {
   const { handleSubmit, form, submitting, handleSkip } = useSetupCompany();
 
   return (
     <Layout.Default>
-      {/* <Title
+      <Title
         {...TITLE_PROPS}
         after={showSkip && <ButtonIcon name="HiX" onClick={handleSkip} />}
-      /> */}
+      />
       <Form form={form} onSubmit={handleSubmit} className={CLASS_NAMES.form}>
         <Field.Input {...COMPANY_NAME_FIELD_PROPS} />
         <Field.Input
